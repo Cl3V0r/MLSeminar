8f1226aa8a0c41846f1f4b80b6bb8a9e24bec140
cv <clemens.vorsmann@t-online.de>
2019-05-24 16:30:46 +0200

    holzmethode aer funktioniert

--- python/nzz_spider.py
+++ python/nzz_spider.py
@@ -1,22 +1,26 @@
 #execute with scrapy runspider nzz_spider.py -o ../data/nzz.json
 import scrapy
+from urllib.parse import urljoin
 
 
 class NZZSpider(scrapy.Spider):
+    i = 0
     name = "titles"
     start_urls = [
         "https://www.nzz.ch/neueste-artikel/"
     ]
 
     def parse(self, response):
+        self.i+=1
         for title in response.xpath("//div[@class='teaser__content']"):
             yield{
                 'title': title.xpath("./a/h2/span/text()").extract_first()
             }
-        next_page = response.xpath(
-            "//div[@class='pagination']/a[@class='pagination__next']/@href").extract_first()
+        
+        next_page = urljoin('https://www.nzz.ch/neueste-artikel',
+                            'https://www.nzz.ch/neueste-artikel/?page='+str(self.i))
         print("NÄCHSTE SEITE", next_page)
-        if next_page is not None:
+        if next_page is not None and self.i<3:
             next_page_link = response.urljoin(next_page)
             yield scrapy.Request(url=next_page_link, callback=self.parse, dont_filter=True)
 
