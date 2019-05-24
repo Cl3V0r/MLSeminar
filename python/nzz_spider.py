83923d1e1479356ebea3bb33d9103045e15af26c
cv <clemens.vorsmann@tu-dortmund.de>
2019-05-24 13:40:30 +0200

    nzz scraping, genaue xml spezifierung noetig

---
+++ python/nzz_spider.py
@@ -0,0 +1,24 @@
+#execute with scrapy runspider nzz_spider.py -o ../data/nzz.json
+import scrapy
+
+
+class NZZSpider(scrapy.Spider):
+    name = "titles"
+    start_urls = [
+        "https://www.nzz.ch/neueste-artikel/"
+    ]
+
+    def parse(self, response):
+        for title in response.xpath("//div[@class='teaser__content']"):
+            yield{
+                'title': title.xpath("./div/text()").extract_first()
+            }
+        next_page = response.xpath(
+            "//div[@class='pagination']/a[@class='pagination__next']/@href").extract_first()
+        print("NÄCHSTE SEITE", next_page)
+        if next_page is not None:
+            next_page_link = response.urljoin(next_page)
+            yield scrapy.Request(url=next_page_link, callback=self.parse, dont_filter=True)
+
+
+
