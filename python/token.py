8f57ef7d887e986ba6c8ad6bf7346186d7c7eb4e
cv <clemens.vorsmann@t-online.de>
2019-06-04 13:21:32 +0200

    presi outline

---
+++ python/token.py
@@ -0,0 +1,16 @@
+#!usr/bin/env python
+#coding:utf8
+#from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.stem.cistem import Cistem
+
+stemmer = Cistem()
+s1 = "Speicherbehältern"
+stemmer.stem(s1)
+s2 = "Grenzpostens"
+stemmer.stem(s2)
+s3 = "Ausgefeiltere"
+stemmer.stem(s3)
+stemmer = Cistem(True)
+stemmer.stem(s1)
+stemmer.stem(s2)
+stemmer.stem(s3)
