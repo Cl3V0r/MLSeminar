6a3905f0f5724eb1ea33452f5b15f1ccf3559baa
cv <clemens.vorsmann@tu-dortmund.de>
2019-06-04 23:49:23 +0200

    paar tests

--- python/tok.py
+++ python/tok.py
@@ -1,17 +1,42 @@
 #!usr/bin/env python
 #coding:utf8
 #from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import TweetTokenizer
 from nltk.stem.cistem import Cistem
+from nltk.corpus import stopwords
+import nltk
+from sklearn.feature_extraction.text import CountVectorizer
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+from pathlib import Path
 
+nltk.download('stopwords')
+tknzr= TweetTokenizer()
 stemmer = Cistem()
-s1 = "Speicherbehältern"
-stemmer.stem(s1)
-s2 = "Grenzpostens"
-stemmer.stem(s2)
-s3 = "Ausgefeiltere"
-stemmer.stem(s3)
-print(s1,s2,s3)
-stemmer = Cistem(True)
-print(stemmer.stem(s1),
-stemmer.stem(s2),
-stemmer.stem(s3))
+file_in = open("../data/postillon.txt", "r")
+file_out = open("../data/postillon_stem.txt", "w")
+for line in file_in:
+    tokenized = tknzr.tokenize(line)
+    for word in tokenized:
+        if word in stopwords.words('german'):
+            tokenized.remove(word)
+        word = stemmer.stem(word)
+    token_text = " ".join(tokenized)   
+    file_out.write(token_text+'\n')      
+file_in.close()
+file_out.close()
+
+data = open("../data/postillon_stem.txt", "r")
+vectorizer = CountVectorizer(max_features=100, ngram_range=(2, 2))
+X = vectorizer.fit_transform(data).toarray()
+print(vectorizer.get_feature_names())
+print(X)
+
+contents = Path("../data/postillon_stem.txt").read_text()
+wordcloud = WordCloud(background_color='white',
+                      width=1200,
+                      height=1000
+                      ).generate(contents)
+plt.imshow(wordcloud)
+plt.axis('off')
+plt.show()
