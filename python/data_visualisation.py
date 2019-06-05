d71a85fd071593e74d50950e1716e0d42a1b1ad0
cv <clemens.vorsmann@tu-dortmund.de>
2019-06-05 22:06:17 +0200

    spass mit wordclouds

--- python/data_visualisation.py
+++ python/data_visualisation.py
@@ -1,14 +1,27 @@
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 from pathlib import Path
+import numpy as np
+from PIL import Image
+from PIL import ImageFilter
 
+trump_mask = np.array(Image.open('../data/pictures/trump_silhouette.png'))
+contents = Path('../build/preprocessed/fake_news_titles.csv').read_text()
+wordcloud = WordCloud(background_color='black',
+                      width=1920,
+                      height=1080,
+                      mask = trump_mask
+                      ).generate(contents)
+plt.imshow(wordcloud,interpolation='bilinear')
+plt.axis('off')
+plt.margins(x=0, y=0)
+plt.savefig("../build/plots/fake_news_titles_wordcloud.pdf")
 
-
-contents = Path('../data/mixed_news/fake_news_titles.csv').read_text()
+contents = Path('../build/preprocessed/real_news_titles.csv').read_text()
 wordcloud = WordCloud(background_color='white',
                       width=1920,
                       height=1080
                       ).generate(contents)
 plt.imshow(wordcloud)
 plt.axis('off')
-plt.savefig("../build/plots/fake_news_titles_wordcloud.pdf")
+plt.savefig("../build/plots/real_news_titles_wordcloud.pdf")
--- python/preprocessing.py
+++ python/preprocessing.py
@@ -1,3 +1,6 @@
+#!usr/bin/env python
+#coding:utf8
+
 import pandas as pd 
 from nltk.tokenize import TweetTokenizer
 from nltk.stem.porter import *
@@ -9,9 +12,27 @@ nltk.download('stopwords')
 tknzr = TweetTokenizer()
 stemmer = PorterStemmer()
 
+#getting to know the dataset
 news = pd.read_csv('../data/mixed_news/news_dataset.csv')
 print(news.keys())
 print(news.head())
+print(news.label.unique())
 
-fake_titles = news[news['label'] == 'fake'].title.dropna()
+#extracting titles for wordcloud visualization
+titles = news.title.dropna()
+
+for t in titles:
+    tokenized = tknzr.tokenize(t)
+    for word in tokenized:
+        if word in stopwords.words('english'):
+            tokenized.remove(word)
+        word = stemmer.stem(word)
+    token_text = " ".join(tokenized)
+    t = token_text       
+
+fake_titles = titles[news['label'] == 'fake']
 fake_titles.to_csv('../build/preprocessed/fake_news_titles.csv',index=False,sep=" ",header=False)
+
+real_titles = titles[news['label'] == 'real']
+real_titles.to_csv('../build/preprocessed/real_news_titles.csv',
+                   index=False, sep=" ", header=False)
